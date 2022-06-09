0358c5d991ac3495fb03701b5d182bcb398668e2
Silvxo <56368014+Silvxo@users.noreply.github.com>
2022-06-09 16:59:41 -0300

    Update JoaquimControl_Forms.ino

--- JoaquimControl_Forms/JoaquimControl_Forms.ino
+++ JoaquimControl_Forms/JoaquimControl_Forms.ino
@@ -3,13 +3,13 @@
 #include <WiFi.h>
 
 #define chaveID "entry.1433246606" // Pergunta 1
-#define ChaveTime "entry.467123702" // Horário
-#define ChaveForms "1FAIpQLSf-EVgZtMlZMmLVBSzfgLeRJin02x6ySvkh4HNnn3vsFh27-Q" //Chave do forms
+#define chaveTime  "entry.467123702" // Horário
+#define chaveForms "1FAIpQLSf-EVgZtMlZMmLVBSzfgLeRJin02x6ySvkh4HNnn3vsFh27-Q" //Chave do forms
 WiFiClientSecure client;
 
-//dados do wifi
+// Dados do wifi
 
-const char* ssid = "sangsunga";
+const char* ssid = "Sangsunga";
 const char* password = "ibis2211";
 
 void setup() {
@@ -18,7 +18,7 @@ void setup() {
   Serial.println("Client has started");
 
   // Conectando na rede
-  WiFi.begin(rede, senha);
+  WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -33,17 +33,17 @@ void loop() {
    if (client.connect("docs.google.com", 443)) {
       String paraEnviar = "GET /forms/d/e/";
       paraEnviar += chaveForms;
-      paraEnviar += "/formsResponse?"
+//      paraEnviar += "/formsResponse?"
       paraEnviar += chaveID;
-      paraEnviar += "="
-      paraEnviar += // Receber ID do leitor
-      paraEnviar += "&"
+      paraEnviar += "=";
+//      paraEnviar += ; //Receber ID do leitor;
+      paraEnviar += "&";
       paraEnviar += chaveTime;
-      paraEnviar += "="
-      paraEnviar += // Receber horário
+      paraEnviar += "=";
+//      paraEnviar += ;// Receber horário
       paraEnviar += "&submit=Submit HTTP/1.1";
       client.println(paraEnviar);
-      client.println("Host: docs.google.com")
+      client.println("Host: docs.google.com");
       client.println();
       client.stop();
       Serial.print("Dados enviados");
@@ -51,6 +51,3 @@ void loop() {
       Serial.print("Erro ao conectar");
    }
 }
-https://docs.google.com/forms/d/e/1FAIpQLSf-EVgZtMlZMmLVBSzfgLeRJin02x6ySvkh4HNnn3vsFh27-Q/viewform?usp=pp_url&entry.1433246606=11609177&entry.467123702=12:30:30
-
-https://docs.google.com/forms/d/e/1FAIpQLScela3DKCOph3kJgsqbcnC31An9tmrq-2sTvAzK3qlPRGnnvQ/viewform?usp=pp_url&entry.513051709=batata
